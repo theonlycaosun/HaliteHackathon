e68e2cea91f9bf81edae6169edbd2ce084fd688f
Michael Caosun <michael_caosun@horacemann.org>
2017-01-29 15:15:24 -0500

    Changes

--- MyBot.cpp
+++ MyBot.cpp
@@ -5,36 +5,17 @@
 #include <time.h>
 #include <set>
 #include <fstream>
+#include <queue>
 
 #include "hlt.hpp"
 #include "networking.hpp"
 
 using namespace hlt;
 
-Site findClosestSite(Site origin)
-{
-    std::queue<Site> Q;
-    std::vector<Site> children;
-    int distance;
-    
-    Q.push(origin);
-    
-    while(!Q.empty())
-    {
-        Site t = Q.front();
-        distance += t.getDistance();
-        
-        Q.pop();
-        
-        if(t == goal){
-            return ;
-        }
+//strength/production
 
-    }
-    return path;
-}
 
-std::vector<Site> getTileWithinDistance(int distance, Location origin) {
+/*std::vector<Site> getTilesWithinDistance(int distance, Location origin) {
     
     std::vector<Location> tiles;
     
@@ -50,16 +31,73 @@ std::vector<Site> getTileWithinDistance(int distance, Location origin) {
         sites.push_back(presentMap.getSite({tiles.at(i).x, tiles.at(i).y}));
     }
     return sites;
-}
+}*/
 
+unsigned char myID;
+hlt::GameMap presentMap;
+
+//lower is better
+/*float evaluate (origin, destination) {
+    if (destination.owner == 0) {
+        return destination.production;
+    }
+    else if (destination.owner == myID){
+        return destination.production;
+    }
+    else {
+        return destination.strength/destination.production;
+    }
+}*/
+
+Move assignMove(Location t) {
+    if (presentMap.getSite(t).strength == 0) {
+        return Move{t, STILL};
+    }
+    else {
+    hlt::Location l;
+        if (presentMap.getSite(t).strength < presentMap.getSite(t).production*5) {
+            return Move{t, STILL};
+        }
+        else {
+            //Breadth First Search
+            if(presentMap.getSite(t).owner == myID) {
+                std::queue<hlt::Location> bfs;
+                std::set<hlt::Location> visited;
+                bfs.push(t);
+                do {
+                    l = bfs.front();
+                    visited.insert(l);
+                    bfs.pop();
+                    
+                    for(int d : CARDINALS) {
+                        hlt::Location n = presentMap.getLocation(l, d);
+                        if(visited.count(n) == 0) bfs.push(n);
+                    }
+                } while(presentMap.getSite(l).owner == myID);
+                if (t.y < l.y) {
+                    return Move{t, SOUTH};
+                }
+                else if (t.y > l.y) {
+                    return Move{t, NORTH};
+                }
+                else if (t.x < l.x) {
+                    return Move{t, EAST};
+                }
+                else if (t.x > l.x) {
+                    return Move{t, WEST};
+                }
+                else return Move{t, WEST};
+            }
+            else return Move{t, WEST};
+        }
+    }
+    return Move{t, WEST};
+}
 
 int main() {
     srand(time(NULL));
 
     std::cout.sync_with_stdio(0);
-
-    unsigned char myID;
-    hlt::GameMap presentMap;
     getInit(myID, presentMap);
     sendInit("MyC++Bot");
 
@@ -71,27 +109,13 @@ int main() {
 
         for(unsigned short a = 0; a < presentMap.height; a++) {
             for(unsigned short b = 0; b < presentMap.width; b++) {
-                hlt::Location t;
-                t.x = b;
-                t.y = a;
-    
-                if (presentMap.getSite({ b, a }).owner == myID) {
-
-                    
-                }
-                
-            }
-//moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
-                    
-            
-                    //Site Grading Function
-                    //Board State Evaluation Function
-                    //Find Closest Border Function
+                hlt::Location t = { b, a };
+                if (presentMap.getSite(t).owner == myID) {
+                    moves.insert(assignMove(t));
                 }
             }
-
+        }
         sendFrame(moves);
     }
-
     return 0;
 }
